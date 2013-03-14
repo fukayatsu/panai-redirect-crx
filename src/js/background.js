857d06a2852b640f8a41554287aaad04607438bf
fukayatsu <fukayatsu@gmail.com>
2013-03-14 22:52:18 +0900

    redirect to specified url

--- src/js/background.js
+++ src/js/background.js
@@ -2,7 +2,27 @@
   chrome.webNavigation.onBeforeNavigate.addListener(function(details) {
     if (details['parentFrameId'] != -1) { return ; }
 
+    var setting = JSON.parse(localStorage.getItem('setting'));
+    if (!setting) return;
+
     var pageUrl = details['url'];
+    var tagId   = details['tabId'];
+    var redirectTo = "";
+    var subUrls = [];
+
+    for (var urlRegExpStr in setting) {
+      if ((subUrls = pageUrl.match(new RegExp(urlRegExpStr)))) {
+        redirectTo = setting[urlRegExpStr];
+        matches = redirectTo.match(/\${[0-9]}/g);
+
+        if (matches) {
+          for (var i = 0; i < matches.length; i++) {
+            redirectTo = redirectTo.split(matches[i]).join(subUrls[i+1]);
+          }
+        }
+        return chrome.tabs.update(tagId, {url: redirectTo});
+      }
+    }
   });
 
   chrome.browserAction.onClicked.addListener(function(tab){
